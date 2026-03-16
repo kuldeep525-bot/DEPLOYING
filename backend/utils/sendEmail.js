3830672126627faaf6723e9b339f2530f1c44d36
git-hub project <sushma97797@gmail.com>
2026-03-16 16:48:43 +0530

    update

--- backend/utils/sendEmail.js
+++ backend/utils/sendEmail.js
@@ -51,7 +51,9 @@
 // // //   }
 // // // }
 
-import SibApiV3Sdk from "@getbrevo/brevo";
+import { createRequire } from "module";
+const require = createRequire(import.meta.url);
+const SibApiV3Sdk = require("@getbrevo/brevo");
 
 export const sendEmail = async ({ to, subject, text }) => {
   const apiInstance = new SibApiV3Sdk.TransactionalEmailsApi();
